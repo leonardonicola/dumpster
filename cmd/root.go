74be616ff2676a14ddd60de041233bbcf37d845b
Leonardo Nicola <leonardonicolares@gmail.com>
2024-10-14 14:40:55 -0300

    docs: remove unecessary comments and flags

--- cmd/root.go
+++ cmd/root.go
@@ -56,18 +56,4 @@ func initConfig() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	viper.BindPFlag("author", rootCmd.PersistentFlags().Lookup("author"))
-	viper.BindPFlag("useViper", rootCmd.PersistentFlags().Lookup("viper"))
-	viper.SetDefault("author", "Leonardo Nicola <leonardonicolares@gmail.com>")
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.dumpster.yaml)")
-	//
-	// // Cobra also supports local flags, which will only run
-	// // when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/start.go
+++ cmd/start.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 Leonardo Nicola
 */
 package cmd
 
@@ -23,14 +23,4 @@ With dumpster you make multiple backups at once in parallel, which allows us to 
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
